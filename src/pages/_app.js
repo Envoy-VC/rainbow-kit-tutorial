783f7aa3707c31cb4e35aa0c29b94d752cd5f9b0
Vedant Chainani <vedantchainani1084@gmail.com>
2023-02-06 11:16:42 +0530

    Add RainbowKit Wallet Integration

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,5 +1,43 @@
-import '@/styles/globals.css'
+// Import Styles
+
+import "@/styles/globals.css";
+import "@rainbow-me/rainbowkit/styles.css";
+
+// RainbowKit Imports
+import { getDefaultWallets, RainbowKitProvider } from "@rainbow-me/rainbowkit";
+
+// Wagmi Imports
+import { configureChains, createClient, WagmiConfig } from "wagmi";
+import {
+  mainnet,
+  polygon,
+  optimism,
+  arbitrum,
+  polygonMumbai,
+} from "wagmi/chains";
+import { alchemyProvider } from "wagmi/providers/alchemy";
+import { publicProvider } from "wagmi/providers/public";
+
+const { chains, provider } = configureChains(
+  [mainnet, polygon, optimism, arbitrum, polygonMumbai],
+  [alchemyProvider({ apiKey: process.env.ALCHEMY_ID }), publicProvider()]
+);
+const { connectors } = getDefaultWallets({
+  appName: "RainbowKit Tutorial",
+  chains,
+});
+const wagmiClient = createClient({
+  autoConnect: false,
+  connectors,
+  provider,
+});
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <WagmiConfig client={wagmiClient}>
+      <RainbowKitProvider modalSize="compact" chains={chains}>
+        <Component {...pageProps} />
+      </RainbowKitProvider>
+    </WagmiConfig>
+  );
 }
